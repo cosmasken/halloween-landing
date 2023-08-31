cc90227f64474d6bdc51c0e7defe9b17f95e3044
cosmasken <15275726+cosmasken@users.noreply.github.com>
2023-08-31 14:52:58 +0300

    Screenshot

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue"
+import Footer from "./components/Footer.vue"
+import Category from "./components/Category.vue";
+import TrickOrTreat from "./components/TrickOrTreat.vue";
+import Subscribe from "./components/Subscribe.vue";
 </script>
 
 <template>
-  <div class="container overflow-x-hidden">
+  <div class=" center p-16px sm:p-32 lg:p-64 overflow-x-hidden">
 <Navbar/>
     <Hero/>
+    <Category/>
+    <TrickOrTreat/>
+    <Subscribe/>
+    <Footer/>
   </div>
 </template>
 
---
+++ src/components/TrickOrTreat.vue
@@ -0,0 +1,63 @@
+<script setup>
+import tor1 from "../assets/tor1-img.png"
+import tor2 from "../assets/tor2-img.png"
+import tor3 from "../assets/tor3-img.png"
+import tor4 from "../assets/tor4-img.png"
+import tor5 from "../assets/tor5-img.png"
+import tor6 from "../assets/tor-6-img.png"
+
+const tots = [
+  {
+    title: 'Candy',
+    image: tor1,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+  {
+    title: 'Ghost',
+    image: tor2,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+  {
+    title: 'Grave',
+    image: tor3,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+  {
+    title: 'House',
+    image: tor4,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+  {
+    title: 'Spider',
+    image: tor5,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+  {
+    title: 'Bone',
+    image: tor6,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id id consequat, eu faucibus nulla. Ultrices mattis lacus, sed maecenas nascetur.'
+  },
+]
+
+</script>
+
+<template>
+  <section id="trick-or-treat" class="flex flex-col items-center py-32">
+    <h2 class="mt-10 mb-28 text-5xl md:text-6xl font-bold text-white">Trick Or Treat</h2>
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10">
+      <div
+          v-for="(tot, i) of tots"
+          :key="tot.title"
+          class="grid place-items-center text-center gap-5 p-8 rounded-md text-white bg-gradient-to-b from-primary-200/20 to-primary-200/10"
+          :class="i === 1 && 'from-primary-200/50 to-primary-200/30 lg:-translate-y-10'
+        "
+      >
+        <div>
+          <img :src="tot.image" alt="">
+        </div>
+        <h3 class="text-2xl lg:text-4xl font-bold">{{ tot.title }}</h3>
+        <p class="w-[90%] hidden md:block">{{ tot.desc }}</p>
+      </div>
+    </div>
+  </section>
+</template>
